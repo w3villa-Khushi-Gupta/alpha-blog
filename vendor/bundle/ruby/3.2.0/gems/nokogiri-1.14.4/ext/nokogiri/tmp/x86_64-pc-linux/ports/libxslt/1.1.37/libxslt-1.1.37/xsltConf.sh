8351f4b31c00cdda42d02c69f81b6634b760c35f
Khushi <khushi.gupta@w3villa.com>
2023-09-30 16:39:28 +0530

    sep-30-23

---
+++ vendor/bundle/ruby/3.2.0/gems/nokogiri-1.14.4/ext/nokogiri/tmp/x86_64-pc-linux/ports/libxslt/1.1.37/libxslt-1.1.37/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/khushi/rails/alpha-blog/vendor/bundle/ruby/3.2.0/gems/nokogiri-1.14.4/ports/x86_64-linux/libxslt/1.1.37/lib"
+XSLT_LIBS="-lxslt -L/home/khushi/rails/alpha-blog/vendor/bundle/ruby/3.2.0/gems/nokogiri-1.14.4/ports/x86_64-linux/libxml2/2.10.4/lib -lxml2 -lz -lm -ldl "
+XSLT_PRIVATE_LIBS="-lm"
+XSLT_INCLUDEDIR="-I/home/khushi/rails/alpha-blog/vendor/bundle/ruby/3.2.0/gems/nokogiri-1.14.4/ports/x86_64-linux/libxslt/1.1.37/include"
+MODULE_VERSION="xslt-1.1.37"
